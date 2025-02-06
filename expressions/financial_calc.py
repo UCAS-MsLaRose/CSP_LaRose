c13d200b854786678e38bd5f777a80fb78c2234a
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-02-06 09:48:38 -0700

    Set up for fincancial calculator

--- expressions/financial_calc.py
+++ expressions/financial_calc.py
@@ -1,1 +1,39 @@
 # Vienna LaRose, Financial Calculator Python
+
+# write a print statement telling the user what the program is (Budget calculator)
+
+# Ask for monthly income (User input)
+income = float(input("What is your monthy income\n"))
+# Ask for rent amount (User input)
+
+# Ask for utilities amount (User input)
+
+# Ask for groceries amount (User input)
+
+# Ask for transportation amount (User input)
+
+# Calculate savings as 10% of income (variable)
+
+# Calcuate spending money income - (rent+utilities+grociers+transportation+savings) (variable)
+
+# Calculate percent of rent (rent/income) (variable)
+
+# Calculate percent of utilities (utilities/income) (variable)
+
+# Calculate percent of groceries (groceries/income) (variable)
+
+# Calculate percent of transportation (transportation/income) (variable)
+
+# Calculate percent of spending (spending/income) (variable)
+
+# tell user category spending amount AND percent for rent ("You spend $XX.XX on rent and that is XX% of your income")
+
+# tell user category spending amount AND percent for utilites ("You spend $XX.XX on utilities and that is XX% of your income")
+
+# tell user category spending amount AND percent for groceries ("You spend $XX.XX on groceries and that is XX% of your income")
+
+# tell user category spending amount AND percent for transportation ("You spend $XX.XX on transportation and that is XX% of your income")
+
+# tell user category spending amount AND percent for spending ("You spend $XX.XX on spending and that is XX% of your income")
+
+# tell user category spending amount AND percent for savings ("You spend $XX.XX on Savings and that is 10% of your income")
