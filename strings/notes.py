acebf6dfc4d139f585fa5423fbbc8f51016745e3
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-02-10 10:02:03 -0700

    Python notes

---
+++ strings/notes.py
@@ -0,0 +1,12 @@
+# Vienna LaRose, Strings Notes Python
+
+# words in programing (data type)"" ''
+
+#name = input("Please give me a number\n").strip().capitalize()
+#print(num)
+#print(name + "2")
+
+sentence = "The quick brown fox jumps over the lazy dog."
+
+print(sentence.find("fox"))
+
