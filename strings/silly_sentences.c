91bccb4ee430b50f11f4d586433f0dab69cd5284
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-02-18 09:07:41 -0700

    set up for project

---
+++ strings/silly_sentences.c
@@ -0,0 +1,13 @@
+//Vienna LaRose, silly sentences C
+#include <stdio.h>
+//string variables for my user inputs (minimum of 3)
+
+int main(void){
+    //introduce user to program. . .tell them what is happening (print statement)
+
+    //create user inputs (print statements with questions AND scanf to collect the info)(same as the number of variables)
+
+    //insert variables into the sentece to show the user (print statement, only 1) example: "hello %s", name
+
+    return 0;
+}
