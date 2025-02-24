7e709c3c000e8fb9e3be6fa0898fb15eef87ce22
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-02-24 09:53:18 -0700

    Notes in python

---
+++ conditinals/notes.py
@@ -0,0 +1,31 @@
+# Vienna LaRose, Conditionals notes Python
+
+#print("Hello, welcome to my program that will sort you into a category.")
+
+#name = input("What is your name:\n")
+
+#if name == "Vienna":
+    #print("Oh your the teacher. . . never mind.")
+#else:
+    #print("You are a student. Welcome to class.")
+
+
+"""
+num = int(input("how many would you like:(give me a number above 0)\n"))
+
+if num == 1:
+    #print("You have asked for an item")
+elif num <= 3:
+    print("You have asked for a couple of the item.")
+elif num <= 5:
+    print("You have asked for a few of the item. . .or your in the south and you asked for a couple.")
+else:
+    print("You have asked for some of the item.")
+"""
+
+name = "Katie"
+
+if "a" in name or "e" in name or "i" in name or "o" in name or "u" in name:
+    print("Your name has a vowel!")
+else:
+    print("Your name dosn't have a vowel.")
