836b7b1646f3015427f2ca8e4e924995f71dc80e
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-04-23 09:19:43 -0600

    Finish JS Notes

--- javascript/notes.js
+++ javascript/notes.js
@@ -1,7 +1,8 @@
 let images = ["https://images.squarespace-cdn.com/content/v1/63c60d44c98af85334537583/644359f9-c213-4d77-bdb0-b78b7f8424f3/Mount_Timpanogos_at_sunset.jpg","https://i.etsystatic.com/37394532/r/il/27e6c7/5018857984/il_fullxfull.5018857984_l9h3.jpg" , "https://www.mountain-forecast.com/system/images/24500/large/Mount-Timpanogos.jpg?1532401039" 
 ]
 function hello(){
-    document.getElementById("title").innerHTML = "Hello World!"
+    let name = window.prompt("What is your name?", "Koro Sensei")
+    document.getElementById("title").innerHTML = "Hello " + name + "!"
 }
 count = 0
 function change(){
@@ -21,7 +22,14 @@ function normal(){
     document.getElementById("btn").style.backgroundColor = "gray"
     document.getElementById("btn").style.color = "black"
 }
+function push(){
+    document.getElementById("btn").style.backgroundColor = "red"
+}
 
 function show(){
     document.getElementById("hidden").style.display = "block"
+}
+
+function pop(){
+    window.alert("For real. Don't click this!")
 }
